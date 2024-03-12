9f405ba2e17998930ac004898259e4a2d6559218
Rajguru077 <asalkarjugal@gmail.com>
2024-03-12 14:40:59 +0530

    commit

--- Numberguessinggame.cpp
+++ Numberguessinggame.cpp
@@ -2,27 +2,26 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-
-int main(){
-
-	int number, guess, tries = 0;
+int main()
+{
+	int num, guess, tries = 0;
 	srand(time(0)); 
-	number = rand() % 100 + 1; 
-	cout << "Guess My number Game\n\n";
-    do
-	{
-		cout <<"Enter a guess between 1 and 100:";
-		cin >> guess;
-		tries++;
+	num= rand() % 100 + 1; 
+	cout << "Guess The Number Game\n";
 
-		if (guess > number)
-			cout << "Too high!\n\n";
-		else if (guess < number)
-			cout << "Too low!\n\n";
+	do{
+	
+	cout <<"Enter a guess between 1 and 100 :";
+	cin >> guess;
+	tries++;
+
+	if (guess > num)
+		cout << "Too high!\n";
+		else if (guess < num)
+		cout << "Too low!\n";
 		else
-			cout <<"\nCorrect! You got it in"<< tries << "guesses!\n";
-}
-     while (guess!=number);
+		cout << "\nCorrect! You got it in " << tries << " guesses!\n";
+	} while (guess != num);
 
 	return 0;
 }
